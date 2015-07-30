7d28bc775a0fa267a6f7f79dee93f0d9e38bc99c
toto <serinee95@gmail.com>
2015-07-30 22:06:26 +0900

    Create powersoftwo.rb

---
+++ powersoftwo.rb
@@ -0,0 +1,13 @@
+def PowersofTwo(num)
+
+  # code goes here
+  
+  
+  
+  return num & (num-1) == 0
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+PowersofTwo(STDIN.gets)           
