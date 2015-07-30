d472d91af9984f0c158358125c6af4c60e110bf7
toto <serinee95@gmail.com>
2015-07-30 21:08:15 +0900

    Create ex_oh.rb

---
+++ ex_oh.rb
@@ -0,0 +1,11 @@
+def ExOh(str)
+
+  # code goes here
+  
+  return str.count("x") == str.count("o")
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ExOh(STDIN.gets)  
