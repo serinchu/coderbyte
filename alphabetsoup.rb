36d8f88604b1118d894327e9098612bd34f1b2bc
toto <serinee95@gmail.com>
2015-07-31 15:02:54 +0900

    Create alphabetsoup.rb

---
+++ alphabetsoup.rb
@@ -0,0 +1,11 @@
+def AlphabetSoup(str)
+
+str.gsub!(' ','')
+  
+  # code goes here
+  return str.split('').sort.join('')
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+AlphabetSoup(STDIN.gets)           
